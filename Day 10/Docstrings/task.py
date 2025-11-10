8cfb432cb2d64ad8b8f57a17804939ec6eb9d43a
Divasdev <sharmadivas881@gmail.com>
2025-11-10 22:49:19 +0530

    Initial commit:

--- Day 10/Docstrings/task.py
+++ Day 10/Docstrings/task.py
@@ -3,10 +3,9 @@ def format_name(f_name, l_name):
     formated_l_name = l_name.title()
     return f"{formated_f_name} {formated_l_name}"
 
+git
 
-formatted_name = format_name("AnGeLa", "YU")
 
-length = len(formatted_name)
 
 
 
