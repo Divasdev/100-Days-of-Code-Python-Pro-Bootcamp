4ebd9a98c9f8f5612f6047a6a7ef8583fba2a110
Divasdev <sharmadivas881@gmail.com>
2025-11-10 22:47:13 +0530

    functions with outputs

---
+++ Day 10/Functions with Outputs/task.py
@@ -0,0 +1,4 @@
+def fomat_name( f_name, l_name ):
+    f_name.title()
+    l_name.title()
+
--- Day 10/Multiple Return Values/task.py
+++ Day 10/Multiple Return Values/task.py
@@ -1,7 +1,13 @@
-def format_name(f_name, l_name):
-    formated_f_name = f_name.title()
-    formated_l_name = l_name.title()
-    return f"{formated_f_name} {formated_l_name}"
+def is_leap(year):
+    if year %400 ==0:
+        return True
+    elif year%100==0:
+           return False
+    elif year %4 ==0:
+            return True
+    else:
+        return False
+print(is_leap(2400))
+print(is_leap(1989))
 
 
-print(format_name("AnGEla", "YU"))
