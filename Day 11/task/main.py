0e8959bdfa208303894b0f4651da2a2842a336aa
Divasdev <sharmadivas881@gmail.com>
2025-11-17 19:31:38 +0530

    BLACKJACK PROJECT

--- Day 11/task/main.py
+++ Day 11/task/main.py
@@ -7,24 +7,60 @@ def calculate_score():
         computer_score = sum(computer_card)
         return user_card,user_score,computer_card,computer_score
 
-user_score ,user_card,computer_score,computer_card =calculate_score()
-print( user_score ,user_card,computer_score,computer_card)
+user_card,user_score,computer_card,computer_score =calculate_score()
+print( user_card,user_score,computer_card,computer_score)
+
+def user_has_blackjack():
+    if len(user_card)==2:
+        if 10 in user_card and 11 in user_card:
+          return  "you win "
+
+
+def computer_has_blackjack():
+    if len(computer_card)==2:
+        if 10 in computer_card and 11 in computer_card:
+         print("Computer has blackjack. You lose.")
+
+result_user=user_has_blackjack()
+result_computer=computer_has_blackjack()
+
+
+def blackjack_result():
+    '''if result_user and result_computer:
+        print("Both have blackjack — Draw")'''
+    if result_user:
+        print(result_user)
+    elif result_computer:
+        print(result_computer)
+    else:
+      print("play again ")
 
 def game_core():
-    ace=11
-    user_choice=int(input("What is your Number?\n"))
-    computer_choice= random.choice(computer_card)
-    if len(user_card) == 2:
-       if user_card == 11 and 10:
-           print("user has blackjack")
-    elif len(computer_card)==2:
-        if computer_card == 11 and 10:
-            print("computer has blackjack")
+    if user_score > 21:
+        if 11 in user_card:
+            if user_score > 21:
+                print("USER LOSE ")
+    else:
+      user_next_move=input("Do you want to get another card? Type 'y' for YES and 'n' for NO")
+      if user_next_move=="y":
+          calculate_score()
+      else:
+          if computer_score<17:
+              calculate_score()
+          elif computer_score>21:
+              print("USER YOU WIN")
+          else:
+              if computer_score<user_score:
+                  print("USER WIN.COMPUTER LOSE ")
+              elif computer_score>user_score:
+                  print("USER LOSE. COMPUTER WINS")
+              else:
+                  print("THE GAME'S DRAW")
 
 
+
+blackjack_result()
 game_core()
-
-
 
 
 
