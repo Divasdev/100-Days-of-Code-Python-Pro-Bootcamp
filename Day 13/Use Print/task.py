cb09c1eceaaff4ea06e3c17591ba687d3d853a0e
Divasdev <sharmadivas881@gmail.com>
2025-11-21 16:29:00 +0530

    debugging

--- Day 13/Use Print/task.py
+++ Day 13/Use Print/task.py
@@ -1,5 +1,5 @@
-word_per_page = 0
+word_page = int(input("Number of words per page: "))
 pages = int(input("Number of pages: "))
-word_per_page == int(input("Number of words per page: "))
-total_words = pages * word_per_page
+total_words = pages * word_page
 print(total_words)
+
